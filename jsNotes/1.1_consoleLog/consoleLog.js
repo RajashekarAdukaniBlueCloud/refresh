d06d8d2a84497cd8aaa5bbbd2b100869a53048a3
Rajashekar Adukani <42172769+bluecloudRaj@users.noreply.github.com>
2019-09-12 13:05:29 +0530

    Section 1

---
+++ jsNotes/1.1_consoleLog/consoleLog.js
@@ -0,0 +1,28 @@
+// Sectioon 1.1 using console.log();
+    console.log("Hello world");
+
+    var foo = 'bar';
+    console.log(foo);
+
+    //concatenation
+    var thisVar =" first Value"
+    var thatValue = 'Second value';
+    console.log(thisVar, thatValue);    
+
+    //placeholders
+    var greet = "Hello", who = "World";
+    console.log(greet, who);;
+
+    //Logging Objects
+    console.log({
+        "email":'',
+        "Group": {},
+        "Id": 33,
+        "IsHiddeninUi": false,
+        "LoginName": '',
+        "PrincipalType":1,
+        "list":{}
+    });
+
+    //Logging HTML elements
+    console.log(document.body)
